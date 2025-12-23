b8d6515ffcc34003b5031ee56af01465e69c8a0b
elandreo <branndedrage@gmail.com>
2025-12-23 21:09:49 +0100

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MantraRiparazione from './MantraRiparazione';
+
+export default function App() {
+  return (
+    <div>
+      <MantraRiparazione />
+    </div>
+  );
+}
