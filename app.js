60af0ade6a2a619d01994b5895f40251193b9cf9
elandreo <branndedrage@gmail.com>
2026-01-18 16:11:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1796,7 +1796,6 @@ function MantraRiparazione() {
       </div>
     </div>
 
-    {showImportRosaModal && (
       {showImportRosaModal && (
         <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50 p-4">
           <div className="bg-white rounded-2xl max-w-2xl w-full p-6">
